846fa557d2c296fb3fab92be91d58e22ead02735
Dominik Burgdoerfer <db@domachine.de>
2015-07-30 17:49:04 +0200

    refactored

--- app/src/main/java/com/crispymtn/cameratest/ImageProcessor.java
+++ app/src/main/java/com/crispymtn/cameratest/ImageProcessor.java
@@ -20,44 +20,8 @@ public class ImageProcessor {
 
     private Context context;
     private int rotation;
+    public static final float MAX_EDGE_LENGTH = 1200.0f;
 
-    private class Size {
-        int width;
-        int height;
-        float scaleRatio;
-
-        public Size() {
-        }
-
-        public Size(int width, int height) {
-            this.width = width;
-            this.height = height;
-        }
-
-        public int getWidth() {
-            return width;
-        }
-
-        public int getHeight() {
-            return height;
-        }
-
-        public void setWidth(int width) {
-            this.width = width;
-        }
-
-        public void setHeight(int height) {
-            this.height = height;
-        }
-
-        public float getScaleRatio() {
-            return scaleRatio;
-        }
-
-        public void setScaleRatio(float scaleRatio) {
-            this.scaleRatio = scaleRatio;
-        }
-    }
 
     public ImageProcessor(Context context) {
         this.context = context;
@@ -68,11 +32,24 @@ public class ImageProcessor {
 
         // Write the image to disk (is already compressed and a JPEG)
         ByteArrayOutputStream imageStream = new ByteArrayOutputStream();
-        Bitmap image = BitmapFactory.decodeByteArray(imageData, 0, imageData.length);
-        image = rotateBitmap(image, imageData);
-//        image = resize(image, 1200, 1200);
-//        image = resize(image, 1200, 1200);
-        image.compress(Bitmap.CompressFormat.JPEG, 100, imageStream);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = false;
+        options.inDither = false;
+        // TODO: Save memory by calculating sample size
+//        options.inSampleSize = inSampleSize;
+        options.inScaled = false;
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+
+        Bitmap image = BitmapFactory.decodeByteArray(imageData, 0, imageData.length, options);
+        Bitmap rotatedImage = rotateBitmap(image, imageData);
+        image.recycle();
+        image = null;
+
+        rotatedImage.compress(Bitmap.CompressFormat.JPEG, 70, imageStream);
+        rotatedImage.recycle();
+        rotatedImage = null;
+
         Uri imageFileURI = writeFileFromData(imageStream, fileBaseName + ".jpg");
 
         return imageFileURI;
@@ -83,66 +60,58 @@ public class ImageProcessor {
     }
 
     private Bitmap rotateBitmap(Bitmap source, byte[] data) {
-        int exifOffset = Exif.getOrientation(data);
-        int rotation = this.rotation;
-        int rotate = 0;
+        int exifRotation = Exif.getOrientation(data); // Rotation data from EXIF
+        int deviceRotation = this.rotation; // Rotation data from device orientation
 
-        if ((rotation < (270 + 45) && rotation > (270 - 45))) {
+        int imageRotation = 0; // Degrees to rotate source image by
+
+        // Get initial rotation from device
+        if (deviceRotation < (270 + 45) && deviceRotation > (270 - 45)) {
             Log.e("BLIMP", "270 degrees ...");
-            rotate = -90;
-        } else if (rotation < (90 + 45) && rotation > (90 - 45)) {
+            imageRotation = -90;
+        } else if (deviceRotation < (90 + 45) && deviceRotation > (90 - 45)) {
             Log.e("BLIMP", "90 degrees ...");
-            rotate = 90;
+            imageRotation = 90;
         }
-        rotate += exifOffset;
-        if (rotate != 0) {
-            Matrix matrix = new Matrix();
-            Size size = resize(source.getWidth(), source.getHeight(), 1200, 1200);
-            matrix.setScale(size.getScaleRatio(), size.getScaleRatio());
-            matrix.postRotate(rotate, size.getWidth() / 2, size.getHeight() / 2);
-            return Bitmap.createBitmap(source, 0, 0, size.getWidth(), size.getHeight(), matrix, false);
-        } else {
-            return source;
+
+        // Add rotation according to EXIF data
+        imageRotation += exifRotation;
+
+        Matrix matrix = new Matrix();
+
+        // Get scale factor for maximum edge length of MAX_EDGE_LENGTH
+        float scaleFactor = getScaleFactor(source.getWidth(), source.getHeight());
+        int newWidth = (int)(source.getWidth() * scaleFactor);
+        int newHeight = (int)(source.getHeight() * scaleFactor);
+
+        Log.e("BLIMP", "Scaling by " + scaleFactor + " new size: " + newWidth + "x" + newHeight);
+
+        matrix.setScale(scaleFactor, scaleFactor);
+
+        if (imageRotation != 0) {
+            matrix.postRotate(imageRotation, newWidth / 2, newHeight / 2);
         }
+
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
     }
 
-    private Size resize(int width, int height, int maxWidth, int maxHeight) {
-        Size size = new Size();
-        if (maxHeight > 0 && maxWidth > 0) {
-            float ratioBitmap = (float) width / (float) height;
-            float ratioMax = (float) maxWidth / (float) maxHeight;
+    private float getScaleFactor(int width, int height) {
+        float scaleRatio = (float) 1.0;
 
-            int finalWidth = maxWidth;
-            int finalHeight = maxHeight;
-            if (ratioMax > 1) {
-                finalWidth = (int) ((float)maxHeight * ratioBitmap);
-            } else {
-                finalHeight = (int) ((float)maxWidth / ratioBitmap);
-            }
-            Log.e("BLIMP", "Scale " + finalWidth + "," + finalHeight);
-            size.setWidth(finalWidth);
-            size.setHeight(finalHeight);
 
-            int maxEdgeLength = 1200;
-            float scaleRatio = (float) 1.0;
-
-            size.setScaleRatio(scaleRatio);
-// If image is smaller than target, don’t scale
-            if(width <= maxEdgeLength && height <= maxEdgeLength) {
-                return size;
-            }
-
-// Otherwise, compute scaling on longer edge
-            if(width >= height) {
-                scaleRatio = maxEdgeLength/width;
-            } else {
-                scaleRatio = maxEdgeLength/height;
-            }
-            size.setScaleRatio(scaleRatio);
-
-            return size;
+        // If image is smaller than target, don’t scale
+        if(width <= MAX_EDGE_LENGTH && height <= MAX_EDGE_LENGTH) {
+            return scaleRatio;
         }
-        return null;
+
+        // Otherwise, compute scaling on longer edge
+        if(width >= height) {
+            scaleRatio = MAX_EDGE_LENGTH/(float)width;
+        } else {
+            scaleRatio = MAX_EDGE_LENGTH/(float)height;
+        }
+
+        return scaleRatio;
     }
 
     // TODO: Query free space before attempting a write
